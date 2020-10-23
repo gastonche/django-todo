160f30f2648bc707614e802938ab92bb7c0d7c6f
= <nganifaith@gmail.com>
2020-10-23 22:56:37 +0100

    todo configurations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+
+class TaskPriority(Enum):
+    HIGH = 'high'
+    MEDIUM = 'medium'
+    LOW = 'low'
+
+
+class TaskStatus(Enum):
+    PENDING = 'pending'
+    STARTED = 'starting'
+    COMPLETED = 'completed'
+    CANCELLED = 'Cancelled'
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    priority = models.CharField(
+        max_length=20,
+        choices=[(tag, tag.value) for tag in TaskPriority],
+        default=TaskPriority.LOW
+    )
+    status = models.CharField(
+        max_length=40,
+        choices=[(state, state.value) for state in TaskStatus],
+        default=TaskStatus.PENDING
+    )
+    created_at = models.DateTimeField('Date Created')
+    updated_at = models.DateTimeField('Last update')
+
+    def __str__(self):
+        return self.name
+
