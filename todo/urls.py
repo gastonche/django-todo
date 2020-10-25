f27eff47106b84e67912c1a4558ea4dfb16640e8
Gaston Che <gastonnkh@gmail.com>
2020-10-25 21:09:10 +0100

    added edit page

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:task_id>/', views.detail, name='details')
+    path('<int:task_id>/', views.detail, name='details'),
+    path('<int:task_id>/update/', views.update, name='update')
 ]
